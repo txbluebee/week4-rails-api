dcd1df1b87382584188951b3cc50d946d17b92f5
Anna Horodetska and Brian Fan <anna+brian@>
2017-07-17 16:46:18 -0700

    add destroy session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.where(email: params[:email]).first
+    @user = User.where(email: params[:email]).first
 
-    if user && user.valid_password?(params[:password])
-      render json: user.as_json(only: [:email, :authentication_token]), status: :created
+    if @user && @user.valid_password?(params[:password])
+      render :create, status: :created
     else
       head(:unauthorized)
     end
